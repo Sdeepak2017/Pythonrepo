e429c7ce1986c9e3e7bb419faf517fe56f524067
Sdeepak2017 <deepak.shivalingu@gmail.com>
2017-04-13 12:43:37 +0530

    Update condtional statement.py

--- condtional statement.py
+++ condtional statement.py
@@ -1,13 +1,13 @@
 a=int(input("enter the value of a"))
 b=int(input("enter the value of b"))
 dict={}
-if(a>10 and b<50):
+if(a>10 and b<250):
 	c=a+b
 	dict['add']= c
 	print("sum of a+b",dict['add'])
-elif(a>50 and b<100):
+elif(a>50 and b<300):
 	c=b*a
 	dict['multiply']= c
 	print("product of a and b",dict['multiply'])
 else:
-	print("please enter the value within 10-100")
+	print("please enter the value within 10-1000")
